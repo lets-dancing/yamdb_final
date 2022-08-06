0022e4299f8031c3630c84c7fc775387b7484210
Александр Пименов <2010pimenoff@gmail.com>
2022-08-06 16:42:33 +0300

    test

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from .models import Category, Comment, Genre, Review, Title
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "category",
+        "year",
+        "description",
+        "genre",
+    )
+
+    list_editable = ("genre",)
+    search_fields = ("description",)
+    list_filter = ("year",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Title)
+admin.site.register(Genre)
+admin.site.register(Comment)
+admin.site.register(Review)
+admin.site.register(Category)
